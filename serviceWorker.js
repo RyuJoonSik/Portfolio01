137b12676911000f08f7adfdf07daf70cbad9bf2
RyuJoonSik <71337000+RyuJoonSik@users.noreply.github.com>
2021-07-19 18:33:46 +0900

    Portfolio01

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,33 +1,37 @@
-// const staticAssets = ['/', '/public/main.js', '/src/assets/json/district.json'];
+const staticAssets = ["./", "./main.js", "./district.json"];
 
-// async function cacheFirst(req) {
-//   const cachedResponse = caches.match(req);
-//   return cachedResponse || fetch(req);
-// }
+async function cacheFirst(req) {
+  console.log("cacheFirst");
+  const cachedResponse = caches.match(req);
+  return cachedResponse || fetch(req);
+}
 
-// async function newtorkFirst(req) {
-//   const cache = await caches.open('dynamic-cache');
+async function newtorkFirst(req) {
+  console.log("newtorkFirst");
+  const cache = await caches.open("dynamic-cache");
 
-//   try {
-//     const res = await fetch(req);
-//     cache.put(req, res.clone());
-//     return res;
-//   } catch (error) {
-//     return await cache.match(req);
-//   }
-// }
+  try {
+    const res = await fetch(req);
+    cache.put(req, res.clone());
+    return res;
+  } catch (error) {
+    return await cache.match(req);
+  }
+}
 
-// self.addEventListener('install', async (event) => {
-//   const cache = await caches.open('static-cache');
-//   cache.addAll(staticAssets);
-// });
+self.addEventListener("install", async (event) => {
+  console.log("install");
+  const cache = await caches.open("static-cache");
+  cache.addAll(staticAssets);
+});
 
-self.addEventListener('fetch', (event) => {
-  // const req = event.request;
-  // const url = new URL(req.url);
-  // if (url.origin === location.url) {
-  //   event.respondWith(cacheFirst(req));
-  // } else {
-  //   event.respondWith(newtorkFirst(req));
-  // }
+self.addEventListener("fetch", (event) => {
+  console.log("fetch");
+  const req = event.request;
+  const url = new URL(req.url);
+  if (url.origin === location.url) {
+    event.respondWith(cacheFirst(req));
+  } else {
+    event.respondWith(newtorkFirst(req));
+  }
 });
