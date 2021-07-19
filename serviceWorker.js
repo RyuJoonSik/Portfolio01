2e778e81024b9059b75db183b7c59132308ec839
RyuJoonSik <71337000+RyuJoonSik@users.noreply.github.com>
2021-07-19 23:51:36 +0900

    Portfolio01

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,13 +1,11 @@
 const staticAssets = ["./", "./main.js", "./district.json"];
 
 async function cacheFirst(req) {
-  console.log("cacheFirst");
   const cachedResponse = caches.match(req);
   return cachedResponse || fetch(req);
 }
 
 async function newtorkFirst(req) {
-  console.log("newtorkFirst");
   const cache = await caches.open("dynamic-cache");
 
   try {
@@ -19,17 +17,24 @@ async function newtorkFirst(req) {
   }
 }
 
+/* install 이벤트는 브라우저가 새로운 서비스 워커를 감지할 때마다 호출 */
 self.addEventListener("install", async (event) => {
   console.log("install");
+  /* 
+    cacheName과 일치하는 Cache 객체를 resolve한 Promise 반환
+    일치하는 Cache가 없을 경우 cacheName의 이름으로 생성된 새로운 Cache객체를 resolve한 Promise 반환
+  */
   const cache = await caches.open("static-cache");
+  /* URL 배열을 가져와 주어진 cache에 응답  */
   cache.addAll(staticAssets);
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log("fetch");
   const req = event.request;
   const url = new URL(req.url);
+  /* url.origin : 도메인 */
   if (url.origin === location.url) {
+    /* 등록된 이벤트에 응답하기 위해 respondWith()를 호출 */
     event.respondWith(cacheFirst(req));
   } else {
     event.respondWith(newtorkFirst(req));
